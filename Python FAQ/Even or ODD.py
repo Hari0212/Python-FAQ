ee2e987438b8f3cf5c9e2b1c7a3f9ec8e77861b4
Hari0212 <163858151+Hari0212@users.noreply.github.com>
2024-07-17 17:13:34 +0530

    Add files via upload

---
+++ Python FAQ/Even or ODD.py
@@ -0,0 +1,5 @@
+a = int(input("Give an NO :"))
+if(a%2==0):
+    print("Its Even")
+else:
+    print("Its ODD")
---
+++ Python FAQ/Max of 2.py
@@ -0,0 +1,6 @@
+a,b = [int(input("Give the nos:"))]
+
+if(a>b):
+    print("A is Greater")
+else:
+    print("B is Greater")
